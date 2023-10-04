5f5930d197afca5acc9562f849f3dc598c9d39f1
gl615s <gl615s@intl.att.com>
2023-10-04 16:35:37 +0300

    politically correcting helo world mesag

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Cyka Blyat !'));
+app.get('/', (req, res) => res.send('Kak Dilla !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
